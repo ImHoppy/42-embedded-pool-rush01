adae94bee91149be93429aa04fb06c89b1f0d858
Hoppy <mbraets@student.42.fr>
2024-04-27 12:41:17 +0200

    Add AHT20 lib

---
+++ lib/aht20.h
@@ -0,0 +1,13 @@
+#include <i2c.h>
+
+typedef struct
+{
+	uint32_t humidity;
+	uint32_t temp;
+} aht20_data;
+
+void aht20_init(void);
+aht20_data aht20_mesure(void);
+
+void print_temp(uint32_t temp);
+void print_humi(uint32_t humi);
